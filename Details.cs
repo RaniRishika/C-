9213ea8005b3cdcbf42b31cccca47396b3e5fce7
Rishika-Kirala <rishikarishikarishi@gmail.com>
2020-01-20 17:13:24 +0530

    first

---
+++ Details.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDetails
+{
+
+    class Details
+    {
+      private  String rollno;
+ private String name { get; set; }
+      private  int sem;
+       private String branch;
+       private double marks;
+        public String Rollno
+        {
+            get
+            {
+                return rollno;
+            }
+            set
+            {
+                rollno = value;
+            }
+        }
+
+       public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Sem
+        {
+            get
+            {
+                return sem;
+            }
+            set
+            {
+                sem = value;
+            }
+        }
+        private string Branch 
+        {
+            get
+            {
+                return branch;
+            }
+
+            set
+            {
+                branch = value;
+            }
+        }
+        public double Marks
+        {
+            get
+            {
+                return marks;
+            }
+        }
+        public Details()
+        {
+            Name = "sravani";
+            rollno = "1";
+            Branch = "cse";
+            sem = 4;
+        }
+        public Details(String name,String rollno,String branch,int sem,int marks)
+        {
+            this.name = name;
+            this.rollno = rollno;
+            this.branch = branch;
+            this.sem = sem;
+            this.marks = marks;
+        }
+        public void AcceptStudentDetails()
+        {
+          
+            Console.WriteLine("student details are accepted");
+            Console.WriteLine("enter marks");
+            this.marks =int.Parse( Console.ReadLine());
+            Console.WriteLine("enter branch");
+            this.branch = Console.ReadLine();
+        }
+        public void DisplayDetails()
+        {
+            
+            Console.WriteLine("student details");
+            Console.WriteLine(this.Name  + "\n" + this.Rollno + "\n" + this.Branch + "\n" + this.Sem + "\n" + this.Marks);
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details d = new Details("sravani","15a81a0589","cse",4,40);
+            Details d1 = new Details("sravan", "15a81a059", "cse", 84, 040);
+            /*d.Name = "sravani";
+              d.Rollno = "15a81a0589";
+              d.Sem = 1;
+              d.AcceptStudentDetails();*/
+            d.DisplayDetails();
+            d1.DisplayDetails();
+            Console.ReadKey();
+        }
+    }
+}
